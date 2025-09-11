b3de695ccb202e2dadc2458df36915d6bb0ae891
Federico Moretti <hello@federicomoretti.it>
2025-09-11 20:22:49 +0200

    chore: remove UUIDv5 support, update feed markup

--- src/utils.ts
+++ src/utils.ts
@@ -30,14 +30,14 @@ export const writeFeed = async (articles: Article[]): Promise<void> => {
   try {
     const entries = (articles: Article[]) => {
       return articles.map((article: Article) => {
-        return `  <entry>\n    <title>${JSON.stringify(article.title).replace(/"/g, '')}</title>\n    <link>${JSON.stringify(article.url).replace(/"/g, '')}</link>\n    <id></id>\n    <updated>${JSON.stringify(article.published_at).replace(/"/g, '')}</updated>\n  </entry>`;
+        return `  <entry>\n    <title>${JSON.stringify(article.title).replace(/"/g, '')}</title>\n    <link rel="alternate" type="text/html" href="${JSON.stringify(article.url).replace(/"/g, '')}"/>\n    <id>${JSON.stringify(article.url).replace(/"/g, '')}</id>\n    <published>${JSON.stringify(article.published_at).replace(/"/g, '')}</published>\n  </entry>`;
       }).join('\n');
     };
     const authorName: string = core.getInput('author-name');
     const authorNickname: string = core.getInput('author-nickname');
     const feedLink: string = core.getInput('feed-link');
-    const data = `<?xml version="1.0" encoding="utf-8"?>\n<feed xmlns="http://www.w3.org/2005/Atom">\n  <title>${authorName} AKA “${authorNickname}”</title>\n  <link rel="self" href="${feedLink}/feed.xml"/>\n  <updated>${new Date().toISOString()}</updated>\n  <id></id>\n${entries(articles)}\n</feed>\n`;
-    await writeFile('feed.xml', data); // TODO: @fedtti
+    const data = `<?xml version="1.0" encoding="utf-8"?>\n<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en">\n  <title>${authorName} AKA “${authorNickname}”</title>\n  <link rel="self" type="application/atom+xml" href="${feedLink}/feed.xml"/>\n  <link rel="alternate" type="text/html" href="${feedLink}/"/>\n  <updated>${new Date().toISOString()}</updated>\n  <id>${feedLink}/feed.xml</id>\n${entries(articles)}\n</feed>\n`;
+    await writeFile('feed.xml', data);
   } catch (error) {
     throw new Error(`Error: ${(error as Error).message}.`);
   }
